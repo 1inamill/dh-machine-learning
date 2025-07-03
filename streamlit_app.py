4e1db83b30679913795ed8dba9771501b90ac4a2
danhino <dan.hinojosa@gmail.com>
2025-07-03 17:13:06 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,7 @@ st.title('🤖 Machine Learning App')
 st.write('This is a machine learning model!')
 
 with st.expander('Data'):
-df = pd.read_csv()
+with st.expander('Data'):
+  st.write('**Raw data**')
+  df = pd.read_csv('https://raw.githubusercontent.com/1inamill/datasets/master/penguins_cleaned.csv')
+  df
